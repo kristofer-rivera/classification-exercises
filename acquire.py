293c59a8286f07e87d55be407210ac4fee50f075
Kristofer Rivera <kristofer.e.rivera@gmail.com>
2022-02-24 17:08:27 -0600

    Adjustments to acquisition functions.

--- acquire.py
+++ acquire.py
@@ -1,27 +1,28 @@
 from env import host, user, password, get_db_url
 import pandas as pd 
-import numpy as np
 import os
 
 # 1.
 
-def get_titanic_data():
+def get_titanic_data(use_cache=True):
     filename = 'titanic.csv'
-    if os.path.isfile(filename):
+    if os.path.isfile(filename) and use_cache:
+        print('Using cached csv...')
         return pd.read_csv(filename)
     else:
         print('Retrieving data from mySQL server...')
         df = pd.read_sql('SELECT * FROM passengers;', get_db_url('titanic_db'))
         print('Caching data as csv file for future use...')
-        df.to_csv(filename)
+        df.to_csv(filename, index=False)
         return df
 
 # 2. 
 
-def get_iris_data():
+def get_iris_data(use_cache=True):
     filename = 'iris.csv'
 
-    if os.path.isfile(filename):
+    if os.path.isfile(filename) and use_cache:
+        print('Using cached csv...')
         return pd.read_csv(filename)
     else:
         print('Retrieving data from mySQL server...')
@@ -29,15 +30,16 @@ def get_iris_data():
         SELECT * FROM measurements 
         JOIN species USING(species_id);''' , get_db_url('iris_db'))
         print('Caching data as csv file for future use...')
-        df.to_csv(filename)
+        df.to_csv(filename, index=False)
         return df
 
 # 3.
 
-def get_telco_data():
+def get_telco_data(use_cache=True):
     filename = 'telco.csv'
 
-    if os.path.isfile(filename):
+    if os.path.isfile(filename) and use_cache:
+        print('Using chaced csv...')
         return pd.read_csv(filename)
     else:
         print('Retrieving data from mySQL server...')
@@ -47,8 +49,7 @@ def get_telco_data():
         JOIN payment_types USING (payment_type_id)
         JOIN internet_service_types USING (internet_service_type_id);''' , get_db_url('telco_churn'))
         print('Caching data as csv file for future use...')
-        df.to_csv(filename)
+        df.to_csv(filename, index=False)
         return df
 
 # 4. All work for this exercise already included in previous exercises
-
